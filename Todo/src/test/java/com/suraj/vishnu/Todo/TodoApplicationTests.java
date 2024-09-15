f8136b893532f9ac4a6c99cfe63e2914ea7c19f2
srjOfficial <suraj2office@duck.com>
2024-09-15 16:00:21 +0530

    creating the project

---
+++ Todo/src/test/java/com/suraj/vishnu/Todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.vishnu.Todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
