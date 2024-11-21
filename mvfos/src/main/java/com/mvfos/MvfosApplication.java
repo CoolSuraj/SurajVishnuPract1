ab439fabfe8b1f8614d3c79520c025bbd3782a4d
vishnu <vishnushankarkumar1@gmail.com>
2024-11-22 01:22:42 +0530

    Initial commit

---
+++ mvfos/src/main/java/com/mvfos/MvfosApplication.java
@@ -0,0 +1,13 @@
+package com.mvfos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvfosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvfosApplication.class, args);
+	}
+
+}
---
+++ mvfos/src/test/java/com/mvfos/MvfosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvfos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvfosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
